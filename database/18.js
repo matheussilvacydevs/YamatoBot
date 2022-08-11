717409fce11dd0e74ff9be19b26ef0c4321120bc
Matheus Silva <88209334+matheussilvacydevs@users.noreply.github.com>
2022-08-11 03:21:55 -0400

    Add files via upload

---
+++ database/18.js
@@ -0,0 +1,45 @@
+  {
+    "text": "Nome : Danca\nFoto : 18\nVídeo : 13\nTamanho : 100 MB\nDuração : - Minutos\nQualidade : HD+\nTaxa : ⭐⭐⭐⭐\n\nLink para download\nDownload : https://drive .google.com/file/d/1hDxgMnQ4ps2DkVhFyc51ApdsavGfqs3B/view\nArquivo de senha: AA18+#38",
+    "image": "https://i.ibb.co/s9dJRr1/image.png"
+    },
+    {
+    "text": "Nome: Mira cinggey\nFotos: 6\nVídeos: 10\nTamanho: 83 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐⭐\n\nLink para download\nDownload: https:// drive.google.com/file/d/1hcr06URPB2ID6EjzXJAXZuOdmtoZkPs9/view\nArquivo de senha: AA18+#39",
+    "image": "https://i.ibb.co/tqXh4c7/image.png"
+    },
+    {
+    "text": "Nome : Dewi\nFotos : 266\nVídeos : 3\nTamanho : 150 MB\nDuração : - Minutos\nQualidade : HD\nTaxa : ⭐⭐⭐⭐\n\nLink para download\nDownload : https://drive .google.com/file/d/1iVfhweyoExOcSCq-LHvJSHRp7pmbNmzg/view\nArquivo de senha: AA18+#40",
+    "image": "https://i.ibb.co/ZBBxgqy/image.png"
+    },
+    {
+    "text": "Nome: Jenn\nFotos: 50\nVídeos: 7\nTamanho: 105 MB\nDuração: - Minutos\nQualidade: HD+\nTaxa: ⭐⭐⭐⭐\n\nLink para download\nDownload: https://drive .google.com/file/d/1jqYvhwGLp8BgTItHRQpONu1MX3o4o12d/view\nArquivo de senha: AA18+#41",
+    "image": "https://i.ibb.co/jWx7DHR/image.png"
+    },
+    {
+    "text": "Nome: Yanna Davud\nFotos: 61\nVídeos: 5\nTamanho: 22 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐⭐\n\nLink para download\nDownload: https:// drive.google.com/file/d/1kIs0lNpZW-R5ub09GiQG6-wgdfagoZsV/view\nArquivo de senha: AA18+#41",
+    "image": "https://i.ibb.co/SRQ590w/image.png"
+    },
+    {
+    "text": "Nome : Nina fika luna\nFotos : 30\nVídeos : 28\nTamanho : 153 MB\nDuração : - Minutos\nQualidade : HD\nTaxa : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https:// /drive.google.com/file/d/1kirVFR8V7aYAvY_6s9v5QKyxy5hRQw-X/view\nArquivo de senha: AA18+#42",
+    "image": "https://i.ibb.co/CsL5XWy/image.png"
+    },
+    {
+    "text": "Nome: Ain Penang\nFotos: 53\nVídeos: 7\nTamanho: 42 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐⭐\n\nLink para download\nDownload: https:// drive.google.com/file/d/1kzGeQKuKaQKiz6u_JEcPh9wuJGKVB-sv/view\nArquivo de senha: AA18+#43",
+    "image": "https://i.ibb.co/zsSh5t8/image.png"
+    },
+    {
+    "text": "Nome: Jilboobs\nFotos: -\nVídeo: 8\nTamanho: 84 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐\n\nLink de download\nDownload: https://drive. google.com/file/d/1l6M6JXmNiHGaybQiHH39wucVhvLkTjkX/view\nArquivo de senha: AA18+#44",
+    "image": "https://i.ibb.co/L9M2pH4/image.png"
+    },
+    {
+    "text": "Nome: Kiki hijab\nFoto: -\nVídeo: 14\nTamanho: 190 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐⭐\n\nLink para download\nDownload: https:// drive.google.com/file/d/1lZDvGBclp35JxhWRpVSCrVOO5YhIQSSn/view\nArquivo de senha: AA18+#45",
+    "image": "https://i.ibb.co/q53mKTp/image.png"
+    },
+    {
+    "text": "Nome: Tifanny\nFoto: 117\nVídeo: 4\nTamanho: 30 MB\nDuração: - Minutos\nQualidade: HD\nTaxa: ⭐⭐⭐⭐\n\nDownload Link\nDownload: https://drive .google.com/file/d/1lrO9YJ1-MJtHavMpN2kG3nmJSReJtzE6/view\nArquivo de senha: AA18+#46",
+    "image": "https://i.ibb.co/12myv6w/image.png"
+    },
+    {
+    "text": "Nome : Vierannii\nFotos : 95\nVídeo : -\nTamanho : 12 MB\nDuração : - Minutos\nQualidade : HD\nTaxa : ⭐⭐⭐⭐\n\nLink para download\nDownload : https://drive .google.com/file/d/1lrOIZFT1n5XWqkX5uVq7xmmSpKjX70Mf/view\nArquivo de senha: AA18+#47",
+    "image": "https://i.ibb.co/yhbnjtR/image.png"
+    }
+]
